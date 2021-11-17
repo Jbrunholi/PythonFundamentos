fd1ef301426d1a2cd6dfd924abc03d0aecb8199a
Jonathan Brunholi Do Nascimento <Jonathanbnasc@gmail.com>
2021-11-17 17:18:55 -0300

    Create IfElseExample.py

---
+++ FlowLoops/IfElseExample.py
@@ -0,0 +1,27 @@
+# if condition:
+#     code to be executed
+
+if 10 < 20:
+    print("Olá Pithon")
+
+
+age = 18
+if age >= 18:
+    print("Bem vindo ao Facebook")
+
+if 0:
+    print("0 example")
+
+if 1:
+    print("0 Demo")     # 1 equivale a True(bool)
+
+# if condition:
+#   code to be executed
+# else:
+#   code to be executed
+
+age = 17
+if age >= 18:
+    print("Bem vindo ao Facebook")
+else:
+    print("Você precisa ter 18 anos para acessar esse conteúdo.")
