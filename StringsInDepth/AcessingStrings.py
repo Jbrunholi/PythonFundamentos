c41d53356a9e471d4cf39a0d518c7bc627a03d56
Jonathan Brunholi Do Nascimento <Jonathanbnasc@gmail.com>
2021-11-17 23:58:32 -0300

    Create AcessingStrings.py

---
+++ StringsInDepth/AcessingStrings.py
@@ -0,0 +1,21 @@
+a = "Python"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+
+print("\n")
+
+print(a[-6])
+print(a[-5])
+print(a[-4])
+print(a[-3])
+print(a[-2])
+print(a[-1])
+
+# len()
+print(len(a))
+
+i = 0
